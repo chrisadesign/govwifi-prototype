ffb154f4b642f15d2c296510e11f0f892e9f1219
Chris Armstrong <mail@chrisarmstrong.io>
2021-11-12 11:28:26 +0000

    Creating a new prototype with the beginning of an admin redesign and a new performance page on the public site

---
+++ app/views/perf-data/v1/_routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = new express.Router()
+
+// Set variables
+router.get('*', function(req, res, next){
+  res.locals['serviceName'] = 'GovWifi'
+  res.locals['status'] = 'signed-out'
+
+  next()
+})
+
+
+
+
+
+// Add your routes above the module.exports line
+module.exports = router
